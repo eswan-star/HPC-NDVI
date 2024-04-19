f1c9d01372ede3f72db4a6ae4f6b6f7a3d44642e
Alexander Connan Johnson <alj888@general-dy-general-cr-7.academic.pcluster>
2024-04-19 14:17:56 -0400

    working build script

--- code/cluster_build.sh
+++ code/cluster_build.sh
@@ -3,6 +3,8 @@ cd ${HOME}/team18_2024/code
 #Load needed modules
 spack load python@3.11.6%gcc@7.3.1 cmake@3.27.7%gcc@7.3.1 libtiff@4.5.1 sqlite@3.43.2%gcc@7.3.1 json-c@0.16 hdf5/43gozkm zlib-ng@2.1.4%gcc@7.3.1
 
+export LD_LIBRARY_PATH=/shared/software/spack/opt/spack/linux-amzn2-skylake_avx512/gcc-7.3.1/libtiff-4.5.1-fiyvp24cb4yzjg5cw6bkvh2x4l2s4ehy/lib64/:${LD_LIBRARY_PATH}
+
 CUR_DIR=`pwd`
 PROJ_DIR=${HOME}/team18_2024/code/proj-9.3.1
 GDAL_DIR=${HOME}/team18_2024/code/gdal-3.8.5
@@ -18,7 +20,6 @@ then
 	cmake .. -DCMAKE_INSTALL_PREFIX=${PROJ_DIR} -DCMAKE_BUILD_TYPE=Release
 	cmake --build .
 	cmake --build . --target install
-
 fi
 
 if [ ! -d ${GDAL_DIR} ]
@@ -31,17 +32,17 @@ fi
 
 #now build and install gdal
 cd ${GDAL_DIR}/build
+
 if [ -f CMakeCache.txt ]
 then
 	rm CMakeCache.txt
 fi
+
 cmake .. -DCMAKE_INSTALL_PREFIX=${GDAL_DIR} -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF -DOGR_BUILD_OPTIONAL_DRIVERS=OFF\
-                -DPROJ_DIR=${PROJ_DIR} -DPROJ_INCLUDE_DIR=${PROJ_DIR}/include -DPROJ_LIBRARY=${PROJ_DIR}/lib64 -DCMAKE_BUILD_TYPE=Release
+                -DPROJ_DIR=${PROJ_DIR} -DPROJ_INCLUDE_DIR=${PROJ_DIR}/include -DPROJ_LIBRARY=${PROJ_DIR}/lib64/libproj.so  -DCMAKE_BUILD_TYPE=Release\
+				-DENABLE_DEFLATE64=OFF
 cmake --build . 
 cmake --build . --target install
 
 #go back where we started
 cd ${CUR_DIR}
-
-
-
